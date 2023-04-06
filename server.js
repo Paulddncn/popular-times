9e39b2d688418f5bb55a5cd49d154b1e79f8e382
Cody <bcuy46@yahoo.com>
2023-04-05 23:48:28 -0400

    updated code to fix routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+require('dotenv').config();
 // import sequelize connection
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
