26a86d9dd37ff86a934b1b1fb96583a141faae1e
Cody <bcuy46@yahoo.com>
2023-04-05 21:25:59 -0400

    updated routes

--- routes/shared.js
+++ routes/shared.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios');
 
-
+router.get('/shared', async (req, res) => {
+    try {
+        const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/shared/1/facebook.json?api-key=${process.env.NYT_API_KEY}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server Error'});
+    }
+});
 
 module.exports = router
