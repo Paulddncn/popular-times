4316d9bf35161f437c6f855d58cec13d8e9c353d
Cody <bcuy46@yahoo.com>
2023-04-06 22:49:49 -0400

    Updated changes

--- controllers/api/viewRoutes.js
+++ controllers/api/viewRoutes.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 router.get('/', async (req, res) => {
     try {
         const response = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${process.env.NYT_API_KEY}`);
-        res.json(response.data)
+        res.render('homepage', data);
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Server Error'});
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const routes = require('./routes');
 require('dotenv').config();
+const exphbs = require('express-handlebars')
+const routes = require('./controllers')
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+var hbs = exphbs.create({});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // sync sequelize models to the database, then turn on the server
 app.listen(PORT, () => {
