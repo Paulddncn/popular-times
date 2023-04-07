1b96ba2d2c57ada7c63ee8135e4b301ee7dbb00e
Paul Duncan <paulddncn@gmail.com>
2023-04-06 20:28:04 -0600

    added news model

---
+++ models/news.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt'); //
+const sequelize = require('../config/connection');
+
+class News extends Model {
+
+  }
+
+
+News.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    titlename: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    
+    },
+    abstract: {
+        type: DataTypes.STRING,
+        allowNull: true,
+     
+      },
+  },
+
+);
+
+module.exports = User;
