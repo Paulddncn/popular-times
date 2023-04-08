89dd556ad8c0b7017370ccc9c0b6633f052f00ff
erichziegler <erich.ziegler11@gmail.com>
2023-04-08 17:14:09 -0600

    Updated schema and seeds for dummy/test user login data

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,36 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-let sequelize;
+const sequelize = new Sequelize(
+    // Database name
+    'times_db',
+    // User
+    'root',
+    // Password
+    'INSERT PASSWORD',
+    {
+      // Database location
+      host: 'localhost',
+      dialect: 'mysql',
+      port: 3306
+    }
+  );
 
-if (process.env.JAWSDB_URL) {
-	sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else { 
-    sequelize = new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USER,
-        process.env.DB_PASS,
-        {
-            host: 'localhost',
-            dialect: 'mysql',
-            port: 3306
-        }
-    );
-}
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+// 	sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else { 
+//     sequelize = new Sequelize(
+//         process.env.DB_NAME,
+//         process.env.DB_USER,
+//         process.env.DB_PASS,
+//         {
+//             host: 'localhost',
+//             dialect: 'mysql',
+//             port: 3306
+//         }
+//     );
+// }
 
 module.exports = sequelize;
