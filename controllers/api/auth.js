a780516896fffc2d90716f500a5a4072fa63bed1
Cody <bcuy46@yahoo.com>
2023-04-12 20:04:30 -0400

    Updated code for Heroku

--- controllers/api/auth.js
+++ controllers/api/auth.js
@@ -4,20 +4,43 @@ const Users = require('../../models/User');
 
 router.post('/login', async (req, res) => {
 	try {
-		console.log(req.body);
-		const users = await Users.findOne({
+		console.log("Login request received with body:", req.body);
+		const user = await Users.findOne({
 			where: {
 				email: req.body.email,
 				password: req.body.password,
 			},
 		});
-		res.status(200).json(users);
+		console.log("User found:", user)
+		req.session.save(() => {
+			req.session.user_id = user.id;
+			req.session.logged_in = true;
+			console.log("Session saved with user ID:", user.id);
+			console.log("Session saved with logged_in:", req.session.logged_in);
+			console.log("Session after save:", req.session);
+			res.status(200).json(user);
+		})
+		
 	} catch (error) {
+		console.error("Error occurred during login:", error);
 		res.status(500).json(error);
 	}
 });
 
+router.post('/logout', (req, res) => {
+	if (req.session.logged_in) {
+	  req.session.destroy(() => {
+		console.log("Session destroyed for user ID:", req.session.user_id);
+		res.status(204).end();
+	  });
+	} else {
+	  console.log("No session to destroy");
+	  res.status(404).end();
+	}
+});
+
 module.exports = router;
+
 
 
 
--- server.js
+++ server.js
@@ -4,18 +4,36 @@ const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
 const path = require('path');
 const Articles = require('./models/articles');
-const authRouter = require('./controllers/api/auth');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 // const { isAuthenticated } = require('./controllers/api/auth');
-// const saveRoutes = require('./controllers/api/save');
+const routes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 var hbs = exphbs.create({});
 
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'))
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -24,8 +42,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', async (req, res) => {
   try {
-    const articles = await Articles.fetchAll(); 
-    res.render('main', { layout: 'index', data: articles }); 
+    const articles = await Articles.fetchAll();
+    res.render('main', { layout: 'index', data: articles, logged_in: req.session.logged_in });
+    console.log(articles); 
   } catch (error) {
     console.error(error);
     res.status(500).send('Internal server error');
@@ -40,8 +59,7 @@ app.get('/login', (req, res) => {
   }
 });
 
-app.use('/api/auth', authRouter);
-// app.use('/api/save', isAuthenticated, saveRoutes); 
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
